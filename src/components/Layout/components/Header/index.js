a82eba643001ecfac6d1f3c1d6a1374f828fee3a
Duy Pham <phduy1905@gmail.com>
2022-05-14 15:14:15 +0700

    header ui p1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import classNames from 'classnames/bind';
 import styles from './Header.module.scss';
+import images from '~/assets/images';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleNotch, faCircleXmark, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 const cx = classNames.bind(styles);
 
 function Header() {
     return (
         <header className={cx('wrapper')}>
-            <div className={cx('inner')}>Logo</div>
+            <div className={cx('inner')}>
+                {/* Logo */}
+                <div className={cx('logo')}>
+                    <img src={images.logo} alt="tiktok logo" />
+                </div>
+                {/* Search input */}
+                <div className={cx('search')}>
+                    <input type="text" placeholder="Search accounts and videos" spellCheck={false} />
+                    <button className={cx('clear-button')}>
+                        <FontAwesomeIcon icon={faCircleXmark} />
+                    </button>
+                    <FontAwesomeIcon className={cx('loading')} icon={faCircleNotch} />
+                    <button className={cx('search-button')}>
+                        <FontAwesomeIcon icon={faMagnifyingGlass} />
+                    </button>
+                </div>
+                <div className="right-container">
+                    <h5>right container</h5>
+                </div>
+            </div>
         </header>
     );
 }
